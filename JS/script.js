f2113c0ed909096c3c7ec142b6cd813edd333b50
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-08 17:36:07 +0100

    added array and html element

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
-console.log('HELLO WORLD')
+//CREO LISTA DELLA SPESA
+const shoppingList = ['yougurt greco','albume','cereali','pasta','latte','passata']
+console.log('array lista',shoppingList);
+//CREO ELEMENTO HTML
+const listElement = '<ul>';
+console.log('elemento',listElement);
+let i = '';
+// CREO CICLO WHILE
+while(i < shoppingList.length){
+    const listItem = `<li>${shoppingList[i]}</li>`;
+    console.log(listItem);
+    i++;
+}
