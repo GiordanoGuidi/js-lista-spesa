b192a126ec7e827f4a474c99501e15178b62926d
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-08 17:54:29 +0100

    printed in page

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,20 @@
 //CREO LISTA DELLA SPESA
 const shoppingList = ['yougurt greco','albume','cereali','pasta','latte','passata']
 console.log('array lista',shoppingList);
+// RECUPERO L'ELEMENTO DALL'HTML
+const listElement = document.getElementById('list-element')
 //CREO ELEMENTO HTML
-const listElement = '<ul>';
-console.log('elemento',listElement);
-let i = '';
+let listItems = '<ul>';
+console.log('elemento',listItems);
+let i = 0;
 // CREO CICLO WHILE
 while(i < shoppingList.length){
-    const listItem = `<li>${shoppingList[i]}</li>`;
-    console.log(listItem);
+    listItems += `<li>${shoppingList[i]}</li>`;
     i++;
 }
+listItems+='</ul>';
+console.log(listItems)
+//STAMPO IN PAGINA
+listElement.innerHTML = listItems;
+
+
